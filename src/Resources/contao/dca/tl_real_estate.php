a61119fdbe27f4e2c1be1f97d23867c84b2eb19a
eki89 <fabian@oveleon.de>
2019-12-19 09:36:51 +0100

    long and lat fields added for postal (hidden marketing)

---
+++ src/Resources/contao/dca/tl_real_estate.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of Contao EstateManager.
+ *
+ * @link      https://www.contao-estatemanager.com/
+ * @source    https://github.com/contao-estatemanager/googlemaps
+ * @copyright Copyright (c) 2019  Oveleon GbR (https://www.oveleon.de)
+ * @license   https://www.contao-estatemanager.com/lizenzbedingungen.html
+ */
+if(ContaoEstateManager\GoogleMaps\AddonManager::valid()) {
+    // Add real estate fields
+    $GLOBALS['TL_DCA']['tl_real_estate']['fields']['plzBreitengrad'] = array
+    (
+        'label'                     => &$GLOBALS['TL_LANG']['tl_real_estate']['plzBreitengrad'],
+        'inputType'                 => 'text',
+        'eval'                      => array('maxlength'=>32, 'tl_class'=>'w50'),
+        'sql'                       => "varchar(32) NOT NULL default ''",
+    );
+
+    $GLOBALS['TL_DCA']['tl_real_estate']['fields']['plzLaengengrad'] = array
+    (
+        'label'                     => &$GLOBALS['TL_LANG']['tl_real_estate']['plzLaengengrad'],
+        'inputType'                 => 'text',
+        'eval'                      => array('maxlength'=>32, 'tl_class'=>'w50'),
+        'sql'                       => "varchar(32) NOT NULL default ''",
+    );
+
+    // Extend default palette
+    Contao\CoreBundle\DataContainer\PaletteManipulator::create()
+        ->addField(array('plzBreitengrad', 'plzLaengengrad'), 'laengengrad', Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_AFTER)
+        ->applyToPalette('default', 'tl_real_estate')
+    ;
+}
